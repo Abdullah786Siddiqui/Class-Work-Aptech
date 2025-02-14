fb9c5081c35cb94c3ef6cb6867754fa919025209
Abdullah Siddiqui <abdullahsidzz333@gmail.com>
2025-02-14 15:58:03 +0500

    push

---
+++ Action.js
@@ -0,0 +1,31 @@
+
+  //////  Regular Expression  ADVANCE JS CLASS 1️⃣ //////
+
+// let checkBtn = document.getElementById("chexkBtn")
+// let Result = document.getElementById("result-div")
+
+// function checkNIC(){
+// let inputVal = document.getElementById("inputVal").value
+//   let indecator = false
+//   let Pattern = /^\d{5}-\d{7}-\d{1}$/;
+//   if(Pattern.test(inputVal)){
+//     Result.textContent = "Your Format is Correct"
+//     indecator = true
+//   }else{
+//     Result.textContent = "Your Format is Incorrect"
+//     indecator = false
+//   }
+//   Result.style.color = indecator ? "green" : "red"
+// }
+
+// checkBtn.addEventListener("click",checkNIC)
+
+// document.getElementById("chexkBtn").addEventListener("click",function () {
+//   let inputVal = document.getElementById("inputVal").value.trim()
+//   let Result = document.getElementById("result-div");
+//   let Pattern = /^\d{5}-\d{7}-\d{1}$/;
+//   let isValid = Pattern.test(inputVal)
+//   Result.textContent = isValid ? "Your Format is Correct" : "Your Format is Incorrect"
+//   Result.style.color = isValid ? "green" : "red"
+  
+// });
